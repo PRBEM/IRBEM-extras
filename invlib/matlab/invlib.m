6b5540b5653f744e0aecd3e593680568d3041c97
tpoiii <tpoiii@80555d2d-b7e3-4ae1-aded-73e86dfc2014>
2010-01-06 14:32:08 +0000

    added omni2uni to invlib.m wrapper
    git-svn-id: svn://svn.code.sf.net/p/irbem/code/extras@128 80555d2d-b7e3-4ae1-aded-73e86dfc2014

--- invlib/matlab/invlib.m
+++ invlib/matlab/invlib.m
@@ -1,5 +1,8 @@
 function varargout = invlib(what,varargin)
-% varargout = invlib(what,varargin)
+% invlib provides access to the irbem/invlib library
+% the following routines are supported: ana_spec_inv, omni2uni
+%
+% ana_spec_inv:
 % fit = invlib('ana_spec_inv',y,dy,Egrid,G,dt,b,Eout,...)
 % calls ana_spec_inv_multi as defined in invlib.pdf
 % y and b are NT x NY, dt is NT x 1
@@ -29,22 +32,47 @@ function varargout = invlib(what,varargin)
 % flux.exp.ell = negative log likelihood of fit
 % flux.exp.weight = weight of fit in combined solution (i.e., in fit.flux)
 % flux.exp.lambda(t,i) = expected counts in t'th sample, i'th channel for fit
+%
+% omni2uni:
+% [uniflux,dloguniflux,result] = invlib('omni2uni',omniflux,dlogomniflux,method,...)
+% calls omni2uni as defined in invlib.pdf
+% (note, if omniflux is a vector, then it'll invert each omniflux)
+% omniflux - Estimated isotropic unidirectional flux 
+%  (for TEM-1 method, use #/cm^2/sr/s/keV)
+% dlogomniflux - relative error for omniflux (dimensionless)
+% method is one of: (required options given in parentheses)
+%  'TEM1' - uses TEM1 analytical climatology for electrons (L, E, B/B0)
+%  'Vampola' - uses Vampola's L-dependent sin^n for electrons (L)
+% key,value options (default given in parens):
+%  'keV' - Energy of particle flux, keV
+%  'MeV' - Energy of particle flux, MeV
+%  'Lm' - McIlwain L of locally mirroring particles, in OPQ
+%  'B/B0' - B/B0 of spacecraft location, in OPQ
+%  'NA' - number of grid points for angular integral (50)
+%  'MaxIter' - maximum number of iterations for minimizer (1,000)
+% keyword options for choice of root finder
+%  'PHS' - Powell's Hybrid method, scaled (default)
+%  'PH' - Powell's Hybrid method
+%  'PHNHS' - Powell's Hybrid method w/ numerical Hessian, scaled 
+%  'PHNH' - Powell's Hybrid method w/ numerical Hessian
+%  'Newton' - Newton's method
+%  'GN' - Pseudo-global Newton's method
+%  'DN' - Discrete Newton's method
+%  'Broyden' - Broyden's algorithm, numerical Hessian
+% uniflux - locally-mirroring unidirectional flux (e.g., in #/keV/cm^2/sr/s)
+% dloguniflux - standard error of natural log of uniflux (dimensionless)
+% result - return codem fro library call
+%  1 - success, 0 or negative is an error code (see invlib.pdf)
 
-if ~libisloaded('invlib'),
-    if ispc,
-        libfile = which('invlib.dll');
-    else
-        libfile = which('invlib.so');
-    end
-    hfile = which('invlib.h');
-    loadlibrary(libfile,hfile,'alias','invlib');
-end
+load_invlib;
 
 varargout = cell(1,nargout);
 
 switch(lower(what)),
     case {'ana_spec_inv'},
         [varargout{:}] = ana_spec_inv(varargin{:});
+    case {'omni2uni'},
+        [varargout{:}] = omni2uni(varargin{:});
     otherwise
         error('%s: Unknown "what": "%s"',mfilename,what);
 end
@@ -219,3 +247,126 @@ end
 if any(~f),
     flux(~f) = exp(q(1)-q(2)*log(Ebreak)-(E(~f)-E0));
 end
+
+function [uniflux,dloguniflux,result] = omni2uni(omniflux,dlogomniflux,method,varargin)
+
+switch(lower(method)),
+    case {'tem1'}, method = -1;
+    case {'vampola'}, method = -2;
+    otherwise
+        error('Unknown method "%s"',method);
+end
+
+% translate varargin
+
+options.keV = nan;
+options.Lm = nan;
+options.BB0 = nan;
+options.NA = 50;
+options.MaxIter = 1e3;
+options.root_finder = 0;
+
+i = 1;
+while i <= length(varargin),
+    switch(lower(varargin{i})),
+        case {'kev'}, 
+            i = i+1;
+            options.keV = varargin{i};
+        case {'mev'}, 
+            i = i+1;
+            options.keV = varargin{i}*1e3;
+        case {'lm'}, 
+            i = i+1;
+            options.Lm = varargin{i};
+        case {'b/b0'}, 
+            i = i+1;
+            options.BB0 = varargin{i};
+        case {'na'}, 
+            i = i+1;
+            options.NA = varargin{i};
+        case {'maxiter'}, 
+            i = i+1;
+            options.MaxIter = varargin{i};
+        case {'phs'},
+            options.root_finder = 0;
+        case {'ph'},
+            options.root_finder = 1;
+        case {'newton'},
+            options.root_finder = 2;
+        case {'gn'},
+            options.root_finder = 3;
+        case {'phnhs'},
+            options.root_finder = 4;
+        case {'phnh'},
+            options.root_finder = 5;
+        case {'dn'},
+            options.root_finder = 6;
+        case {'broyden'},
+            options.root_finder = 6;
+    end
+    i = i+1;
+end
+
+all_int_params = cell(5,1);
+all_real_params = cell(3,1);
+
+all_int_params{1+0} = options.NA;
+all_int_params{1+1} = method;
+all_int_params{1+2} = 0; % verbose setting = no text output
+all_int_params{1+3} = options.root_finder;
+all_int_params{1+4} = 1000; % maximumn # of iterations */
+
+all_real_params{1+0} = options.keV;
+all_real_params{1+1} = options.BB0;
+all_real_params{1+2} = options.Lm;
+
+load_invlib;
+
+int_params = nan(size(all_int_params));
+real_params = nan(size(all_real_params));
+nullPtr = libpointer('cstring'); % empty pointer to char *
+unifluxPtr = libpointer('doublePtr',NaN); % pointer to one double
+dlogunifluxPtr = libpointer('doublePtr',NaN); % pointer to one double
+
+N = numel(omniflux);
+if numel(dlogomniflux)==1,
+    dlogomniflux = repmat(dlogomniflux,size(omniflux));
+end
+uniflux = nan(size(omniflux));
+dloguniflux = nan(size(omniflux));
+result = nan(size(omniflux));
+
+for i = 1:N,
+    for j = 1:length(int_params),
+        if numel(all_int_params{j})==1,
+            int_params(j) = all_int_params{j}(1);
+        else
+            int_params(j) = all_int_params{j}(i);
+        end
+    end
+    for j = 1:length(real_params),
+        if numel(all_real_params{j})==1,
+            real_params(j) = all_real_params{j}(1);
+        else
+            real_params(j) = all_real_params{j}(i);
+        end
+    end
+    unifluxPtr.value=NaN;
+    dlogunifluxPtr.value=NaN;
+    result(i) = calllib('invlib','omni2uni',omniflux(i),dlogomniflux,int_params,real_params,nullPtr,unifluxPtr,dlogunifluxPtr);
+    uniflux(i) = unifluxPtr.value;
+    dloguniflux(i) = dlogunifluxPtr.value;
+end
+
+function load_invlib
+% smartly load invlib DLL (.dll or .so)
+if ~libisloaded('invlib'),
+    if ispc,
+        libfile = which('invlib.dll');
+    else
+        libfile = which('invlib.so');
+    end
+    hfile = which('invlib.h');
+    loadlibrary(libfile,hfile,'alias','invlib');
+end
+
--- invlib/matlab/invlib_omni2uni.m
+++ invlib/matlab/invlib_omni2uni.m
@@ -1,20 +1,24 @@
 % demo of omni2uni, same as omni2uni_test.c
 
+omniflux = 1.0E4; % typical value 1E+4 */
+dlogomniflux = log(2)/2;
+E = 300; % keV
+BB0 = 4000.0/100.0; % B/B0
+Lm = 6.6;
+
 int_params = nan(5,1);
 real_params = nan(3,1);
 
 int_params(1+0) = 50; % NA - number of angular gridpoints */
 int_params(1+1) = -1; % TEM-1 method */
 int_params(1+2) = 0; % verbose = 0, not text output */
-int_params(1+3) = 3; % minimizer, 0=BFGS, 3=NM */
-int_params(1+4) = 1000; % maximumn # of iterations */
+int_params(1+3) = 0; % root finder, default */
+int_params(1+4) = 10e3; % maximumn # of iterations */
 
-real_params(1+0) = 300.0; % 300 keV */
-real_params(1+1) = 40000.0/100.0; % B/B0 */
-real_params(1+2) = 6.6; % Lm */
+real_params(1+0) = E; % 300 keV */
+real_params(1+1) = BB0; % B/B0 */
+real_params(1+2) = Lm; % Lm */
 
-omniflux = 1.0E4; % typical value 1E+4 */
-dlogomniflux = log(2)/2;
 
 if ~libisloaded('invlib'),
     loadlibrary('invlib','invlib.h','alias','invlib'); % load the library
@@ -29,6 +33,23 @@ result = calllib('invlib','omni2uni',omniflux,dlogomniflux,int_params,real_param
 uniflux = unifluxPtr.value;
 dloguniflux = dlogunifluxPtr.value;
 
-disp(sprintf('%s:inputs = omni=%.5e (dlog=%.5e) @ [%.1f keV, B/B0=%.2f, Lm=%.2f]',...
-    mfilename,omniflux,dlogomniflux, real_params(1+0),real_params(1+1),real_params(1+2)));
-disp(sprintf('%s:result = %i, uni=%.5e (dlog=%.5e)',mfilename,result,uniflux,dloguniflux));
+fprintf('---------------Manual lib call----------------\n');
+fprintf('%s:inputs = omni=%.5e (dlog=%.5e) @ [%.1f keV, B/B0=%.2f, Lm=%.2f]\n',...
+    mfilename,omniflux,dlogomniflux,E,BB0,Lm);
+fprintf('%s:result = %i, uni=%.5e (dlog=%.5e)\n',mfilename,result,uniflux,dloguniflux);
+
+fprintf('---------------Wrapper call----------------\n');
+
+[uniflux,dloguniflux,result] = invlib('omni2uni',omniflux,dlogomniflux,'TEM1',...
+    'keV',E,'B/B0',BB0,'Lm',Lm);
+fprintf('%s:inputs = omni=%.5e (dlog=%.5e) @ [%.1f keV, B/B0=%.2f, Lm=%.2f]\n',...
+    mfilename,omniflux,dlogomniflux,E,BB0,Lm);
+fprintf('%s:result = %i, uni=%.5e (dlog=%.5e)\n',mfilename,result,uniflux,dloguniflux);
+
+fprintf('---------------Multi call----------------\n');
+s = (90:110)'/100;
+[uniflux,dloguniflux,result] = invlib('omni2uni',omniflux*s,dlogomniflux,'TEM1',...
+    'keV',E*s,'B/B0',BB0*s,'Lm',Lm*s);
+for i = 1:length(s),
+    fprintf('s=%g:result = %i, uni=%.5e (dlog=%.5e)\n',s(i),result(i),uniflux(i),dloguniflux(i));    
+end
