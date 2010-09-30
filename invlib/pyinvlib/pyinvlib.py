4f0f1fae1cd72d218d473b3d5893c6c65d9ab18a
stevemorley <stevemorley@80555d2d-b7e3-4ae1-aded-73e86dfc2014>
2010-09-30 17:30:15 +0000

    pyinvlib updated for efficient subclassing and extended error trapping
    git-svn-id: svn://svn.code.sf.net/p/irbem/code/extras@213 80555d2d-b7e3-4ae1-aded-73e86dfc2014

--- invlib/pyinvlib/pyinvlib.py
+++ invlib/pyinvlib/pyinvlib.py
@@ -3,11 +3,20 @@
 
 """Python interface to the IRBEM InvLib library
 
+Calls to ana_spec_inv and omni2uni are currently supported
+Angular inversions and spectral inversions are performed using
+different classes. It has not yet been implemented, but the plan
+is to have a AngInv object as a possible input to a SpecInv
+object, such that the fluxes, etc are easy to input for the 
+spectral inversion.
+Running this wrapper from the command line calls a test suite to 
+reproduce the specinv_test and omni2uni_test where the setup and 
+function calls are from within Python. The test suite also tests
+that the class is working correctly and should be run for both 
+Python 2.6+ and 3.X whenever changes are made.
+
 Author: Steve Morley, Los Alamos National Laboratory (smorley@lanl.gov)
 Date Created: 23 Sept. 2010
-
-Running this wrapper from the command line calls a test to reproduce the specinv_test
-where the setup and function call are from within Python
 
 To Do:
 Enable read support for PRBEM response files
@@ -15,12 +24,13 @@ Finalize read support for LANL standard response files
 Generalize calling of ana_spec_inv
 Add support for calling of ana_spec_inv_multi (within extant functions)
 Add support for calling pc_spec_inv (and _multi)
+Improve error trapping
 Update unit tests for additional code
 Documentation
 Angular Inversion code (add wide2uni)
 """
 
-import ctypes, ctypes.util, os, sys, csv, math, numbers
+import ctypes, os, sys, csv, math, numbers
 
 if sys.platform == 'linux2':
     ext = 'so'
@@ -31,7 +41,6 @@ else:
 
 floc = os.path.split(locals()['__file__'])
 libpath = floc[0]
-
 
 ##following two functions can be removed when numpy1.5 compatibility in Python3 is tested
 def ravel(mylist):
@@ -49,10 +58,9 @@ def transpose(arr):
 #----------------------
 
 
-class SpecInv(object):
-    """Spectral Inversion class using INVLIB
-    """
-    def __init__(self, verbose=True, rme=0.511):
+class InvBase(object):
+    #base class for INVLIB calls
+    def __init__(self, verbose=True):
         try:
             self._invlib = ctypes.CDLL(os.path.join(libpath,'invlib.'+ext))
         except: #case for testing in IRBEM dist
@@ -63,29 +71,17 @@ class SpecInv(object):
             self._verb = 0
         else:
             self._verb = verbose
-        
-        #set some defaults
-        self._int_params = [None]*10
-        self._real_params = [None]*10 
-        self.rme = rme #defaults to electron with all units in MeV
-        self._default_Eout = [  0.1       ,   0.1274275 ,   0.16237767,   0.20691381,
-         0.26366509,   0.33598183,   0.42813324,   0.54555948,
-         0.6951928 ,   0.88586679,   1.12883789,   1.43844989,
-         1.83298071,   2.33572147,   2.97635144,   3.79269019,
-         4.83293024,   6.15848211,   7.8475997 ,  10.        ]
-        
-        return None
+            
+        self.H = None
     
     def readLANLResp(self, fname):
-        #read 'standard' LANL format response function files
+        """read 'standard' LANL format response function files"""
         #modified port of r_sdtfrmt_resp in papco
-        
         #currently data is nested lists (row major)
         #this should be put into numpy arrays (ensuring Py3k compliance)
 
         fobj = open(fname, 'r')
         header, data, Earray = [], [], []
-        
         c_symb = ';'
         for line in fobj:
             if c_symb in line: #if prefixed by ';' comment symbol it's header
@@ -105,9 +101,8 @@ class SpecInv(object):
         self.H = data
         self.Hgrid = Earray
         self.Hcols = col_hdr
-
-        if self._verb: print('LANL response file read:\n%s' % header[:2])
         
+        if self._verb: print('LANL response file read:\n%s' % header[:2])
         try:
             assert self.Hgrid==self.Egrid
         except AttributeError:
@@ -116,6 +111,45 @@ class SpecInv(object):
             raise UserWarning('Response function not on same energy grid as data')
 
         return None
+        
+    def readRespFunc(self, fname=None, std='LANL'):
+        #method stub for reading PRBEM and LANL format response functions
+        if std.upper() not in ['LANL','PRBEM']:
+            raise ValueError('Unknown response file format specified')
+        if fname == None or not os.path.exists(fname):
+            raise IOError('Requested file %s does not exist' % (fname))
+        if std.upper()=='LANL':
+            rfun = self.readLANLResp(fname)
+            #need to work out how to get this into format required by *_spec_inv
+            #and wide2uni
+            #currently hacked into readLANLResp - good spot??
+        elif std.upper()=='PRBEM':
+            try:
+                from spacepy import pycdf
+                rfun = pycdf.CDF(fname)
+                #now get appropriate numbers from CDF to pass to *_spec_inv
+                #then set self.H
+            except ImportError:
+                raise ImportError('''SpacePy is not installed; CDF support is unavailable without SpacePy\n
+                Please get SpacePy from http://spacepy.lanl.gov''')
+        
+        return None
+        
+    
+class SpecInv(InvBase):
+    """Spectral Inversion class using INVLIB
+    """
+    def __init__(self, rme=0.511, **kwargs):
+        super(SpecInv, self).__init__(self, **kwargs)
+        #set some defaults
+        self._int_params = [None]*10
+        self._real_params = [None]*10 
+        self.rme = rme #defaults to electron with all units in MeV
+        self._default_Eout = [  0.1       ,   0.1274275 ,   0.16237767,   0.20691381,
+         0.26366509,   0.33598183,   0.42813324,   0.54555948,
+         0.6951928 ,   0.88586679,   1.12883789,   1.43844989,
+         1.83298071,   2.33572147,   2.97635144,   3.79269019,
+         4.83293024,   6.15848211,   7.8475997 ,  10.        ]
 
     def readTestInput(self, verbose=True, func=1+2, minim=0, niter=1000):
         #port of specinv_test.c
@@ -146,7 +180,6 @@ class SpecInv(object):
         dum = dum+(NE*NC)
         self.b = datlist[dum:dum+NC]
         self.Eout = datlist[dum+NC:]
-        
         print('Python Analytic Spectral Inversion test: NC=%d, NE=%d, NEout=%d' % (NC, NE, NEout))
         
         self._int_params[0] = NC
@@ -157,31 +190,7 @@ class SpecInv(object):
         self._int_params[5] = niter
         self._int_params[6] = self._verb
         self._int_params[7:] = [0,0,0]
-
         self.setParams()
-        
-        return None
-    
-    def readRespFunc(self, fname=None, std='LANL'):
-        #method stub for reading PRBEM and LANL format response functions
-        if std.upper() not in ['LANL','PRBEM']:
-            raise ValueError('Unknown response file format specified')
-        if fname == None or not os.path.exists(fname):
-            raise IOError('Requested file %s does not exist' % (fname))
-
-        if std.upper()=='LANL':
-            rfun = self.readLANLResp(fname)
-            #need to work out how to get this into format required by *_spec_inv
-            #currently hacked into readLANLResp - good spot??
-        elif std.upper()=='PRBEM':
-            try:
-                from spacepy import pycdf
-                rfun = pycdf.CDF(fname)
-                #now get appropriate numbers from CDF to pass to *_spec_inv
-                #then set self.H
-            except ImportError:
-                raise ImportError('''SpacePy is not installed; CDF support is unavailable without SpacePy\n
-                Please get SpacePy from http://spacepy.lanl.gov''')
         
         return None
         
@@ -193,8 +202,7 @@ class SpecInv(object):
             #NC = 1 #how to calc number of channels?
             NE = int(len(self.Egrid))
         except (AssertionError, AttributeError):
-            raise TypeError('Counts, Relative Error on Counts and Energy grid must be defined')
-        
+            raise TypeError('Counts, Relative Error on Counts and Energy grid must be defined') 
         try:
             assert self.H
         except:
@@ -214,8 +222,7 @@ class SpecInv(object):
             Eout = Eout(*self.Eout)
             NEout = int(len(Eout))
         except AttributeError:
-            NEout = int(NEout)
-            
+            NEout = int(NEout) 
             Eout = NEout*ctypes.c_double
             Eout = Eout(*self._default_Eout)  ##set default log grid of 20 energies
         
@@ -266,7 +273,6 @@ class SpecInv(object):
     def anaSpecInv(self, restmass=0.511):
         '''Analytic spectral inversion'''
         assert self._params
-
         retval = self._invlib.ana_spec_inv(*self._params)
         print('Analytic Spectral Inversion: %s\n' % self.retCodeAna(retval))
         
@@ -278,7 +284,7 @@ class SpecInv(object):
             Eout = list(self._params[-5])
             flux = list(self._params[-4])
             dlogflux = list(self._params[-3])
-        
+            
         outlist=[]
         for eo, fl, dlf in zip(Eout, flux, dlogflux):
             #add time to 1st position in nested list
@@ -290,32 +296,18 @@ class SpecInv(object):
         return None
 
 
-class AngInv(object):
+class AngInv(InvBase):
     #interface to omni2uni and wide2uni functions from invlib
-    def __init__(self, verbose=True):
-        try:
-            self._invlib = ctypes.CDLL(os.path.join(libpath,'invlib.'+ext))
-        except: #case for testing in IRBEM dist
-            self._invlib = ctypes.CDLL('../invlib.'+ext)
-        if verbose==True:
-            self._verb = 1
-        elif verbose==False:
-            self._verb = 0
-        else:
-            self._verb = verbose
-            
+    def __init__(self, **kwargs):
+        super(AngInv, self).__init__(self, **kwargs) 
+        #set null params for later population
         self._int_params = [None]*5
         self._real_params = [None]*3
-        
-        #set null params for later population
         self.omniflux = None
         self.domniflux = None
         self.wideflux = None
         self.dwideflux = None
         self.PAgrid = None
-        self.H = None
-        
-        return None
     
     def retCodes(self, retval):
         #maybe replace this with defined exceptions
@@ -334,17 +326,14 @@ class AngInv(object):
             
     def testOmni2Uni(self):
         #run the test case given in omni2uni     
-        
-        self._int_params[0] = 50#; /* NA - number of angular gridpoints */
-        self._int_params[2] = 1#; /* 1 = verbose to standard out */
-        self._int_params[3] = 3#; /* minimizer, 0=BFGS, 3=NM */
-        self._int_params[4] = 1000#; /* maximumn # of iterations */
-
-        self._real_params[0] = 300.0#; /* 300 keV */
-        self._real_params[1] = 40000.0/100.0#; /* B/B0 */
-        self._real_params[2] = 6.6#; /* Lm */
-
-        self.omniflux = 10000#; /* typical value 1E+4 */
+        self._int_params[0] = 50 #; /* NA - number of angular gridpoints */
+        self._int_params[2] = 1 #; /* 1 = verbose to standard out */
+        self._int_params[3] = 3 #; /* minimizer, 0=BFGS, 3=NM */
+        self._int_params[4] = 1000 #; /* maximumn # of iterations */
+        self._real_params[0] = 300.0 #; /* 300 keV */
+        self._real_params[1] = 40000.0/100.0 #; /* B/B0 */
+        self._real_params[2] = 6.6 #; /* Lm */
+        self.omniflux = 10000 #; /* typical value 1E+4 */
         self.domniflux = math.log(2)/2 #natural log
         
         #run case 1
@@ -358,13 +347,17 @@ class AngInv(object):
         
     def setParams(self, method='TEM1', alpha=5):
         if method.upper() not in ['TEM1', 'VAM']:
-            raise ValueError('Invalid angular inversion method requested')
-        
+            raise ValueError('Invalid angular inversion method requested')       
         if method.upper()=='TEM1':
             self._int_params[1]=-1
         elif method.upper()=='VAM':
             self._int_params[1]=-2
-            
+        #test for presence of inputs
+        if self.omniflux is None or self.domniflux is None:
+            raise ValueError('Flux or Error on flux undefined')
+        if None in self._int_params:
+            raise ValueError('Integer parameters not defined')
+        
         intp = ctypes.c_long*5
         self._int_params = [int(j) for j in self._int_params]
         intp = intp(*self._int_params)
@@ -402,10 +395,10 @@ class AngInv(object):
             if method != None:
                 self.setParams(method=method)
             else:
-                self.setParams()
-        
+                self.setParams() 
         retval = self._invlib.omni2uni(*self._paramsO)
         print('omni2uni Angular Inversion: %s\n' % self.retCodes(retval))
+        
         return retval
 
     def wide2uni(self, method=None):
@@ -416,11 +409,11 @@ class AngInv(object):
                 self.setParams(method=method)
             else:
                 self.setParams()
-        
         retval = self._invlib.wide2uni(*self._paramsW)
         print('wide2uni Angular Inversion: %s\n' % self.retCodes(retval))
+        
         return retval
-    
+        
 
 if __name__=='__main__':
     #run test suite if called from command line
